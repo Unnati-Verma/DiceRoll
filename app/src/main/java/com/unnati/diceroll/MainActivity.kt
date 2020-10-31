b03ea05012517d1158cbfb2d75398dc1b77a123b
Unnati Verma <unnatiwork18@gmail.com>
2020-10-31 18:08:14 +0530

    Initial commit

---
+++ app/src/main/java/com/unnati/diceroll/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.unnati.diceroll
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+
+/* these lines allows user to roll the dice and show on the screen */
+class MainActivity : AppCompatActivity() {
+    lateinit var diceImage:ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.imageView)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice(diceImage)
+        }
+    }  }
+//roll the dice and show on the screen
+private fun rollDice(diceImage: ImageView) {
+    val dice = Dice(6)
+    val diceRoll = dice.roll()
+    val drawableResource = when (diceRoll) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    diceImage.setImageResource(drawableResource)
+    diceImage.contentDescription = diceRoll.toString()
+}
+
+
+
+
+class Dice(val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
